983819ab79d9d2f23725cc449de654aafddc3c98
Stephanie Opiyo <36300402+stephieopiyo@users.noreply.github.com>
2021-06-03 16:48:22 +0300

    changes made

--- src/router.js
+++ src/router.js
@@ -1,14 +1,46 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Recipes from "@/components/Recipes/Recipes";
+import RecipeDetail from "@/components/Recipes/RecipeDetail";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
-      name: "Hello",
-      component: Hello
+      redirect: "/Home"
+    },
+    {
+      path: "/Home",
+      name: "Home",
+      component: () => import("./views/Home.vue")
+    },
+    {
+      path: "/About",
+      name: "About",
+      component: () => import("./views/About.vue")
+    },
+    {
+      path: "/Contact",
+      name: "Contact",
+      component: () => import("./views/Contact.vue")
+    },
+    {
+      path: "/Recipes",
+      name: "Recipes",
+      component: Recipes
+    },
+    {
+      path: "/Recipe/:id",
+      name: "RecipeDetail",
+      component: RecipeDetail
+    },
+    {
+      path: "*",
+      component: () => import("./views/PageNotFound.vue")
     }
   ]
 });
